2dc62e3c5658ba674f3ccbf0cd430543eb30894f
Tom Donohue <monodot@users.noreply.github.com>
2021-03-22 14:54:40 +0000

    Update Application.java

--- src/main/java/xyz/tomd/demos/hellorestjava/Application.java
+++ src/main/java/xyz/tomd/demos/hellorestjava/Application.java
@@ -23,7 +23,7 @@ public class Application {
                                     "<body>" +
                                     "<h1>Hello, world!</h1>" +
                                     "<p>Some extra text here, for your enjoyment!!!</p>" +
-                                    "<p>Soviet Science Fiction movies are the best.</p>" +
+                                    "<p>Driller Killer is the best awful horror movie.</p>" +
                                     "</body>" +
                                     "</html>");
                 }).build();
